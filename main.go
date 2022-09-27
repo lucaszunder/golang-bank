823151a8a512c76d5dc3e15e2d005c1f4c9c20bd
Lucas Zunder <lucas.zunder@hotmail.com>
2022-09-26 22:20:12 -0300

    feat: add feature to pay debts

--- main.go
+++ main.go
@@ -7,10 +7,20 @@ import (
 	c "github.com/lucaszunder/banco/customer"
 )
 
+func payDebt(account verifyAccountType, value float64) {
+	account.Withdraw(value)
+}
+
+type verifyAccountType interface {
+	Withdraw(value float64) string
+}
+
 func main() {
 	owner := c.Owner{Name: "Lucas Zunder", CPF: "000888123", Job: "Engenheiro de software"}
 	account := a.Account{Owner: owner, AgencyNumber: 589, AccountNumber: 4397}
 	account.Deposit(100)
+
+	payDebt(&account, 50.0)
 
 	fmt.Println(account.Balance())
 }
